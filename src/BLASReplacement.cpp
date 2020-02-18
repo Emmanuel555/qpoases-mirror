1a7f3958fb4c22351258b24942ef1f60358112ef
Baskin Senbaslar <basbursen@gmail.com>
2020-02-17 17:03:13 -0800

    pff

--- src/BLASReplacement.cpp
+++ src/BLASReplacement.cpp
@@ -37,60 +37,60 @@
 namespace qpOASES { // as it should have been!!!
 
 
-extern "C" void dgemm_(	const char* TRANSA, const char* TRANSB,
-						const la_uint_t* M, const la_uint_t* N, const la_uint_t* K,
-						const double* ALPHA, const double* A, const la_uint_t* LDA, const double* B, const la_uint_t* LDB,
-						const double* BETA, double* C, const la_uint_t* LDC
-						)
-{
-	la_uint_t i, j, k;
+// extern "C" void dgemm_(	const char* TRANSA, const char* TRANSB,
+// 						const la_uint_t* M, const la_uint_t* N, const la_uint_t* K,
+// 						const double* ALPHA, const double* A, const la_uint_t* LDA, const double* B, const la_uint_t* LDB,
+// 						const double* BETA, double* C, const la_uint_t* LDC
+// 						)
+// {
+// 	la_uint_t i, j, k;
 
-	if ( REFER_NAMESPACE_QPOASES isZero(*BETA) == REFER_NAMESPACE_QPOASES BT_TRUE )
-		for (k = 0; k < *N; k++)
-			for (j = 0; j < *M; j++)
-				C[j+(*LDC)*k] = 0.0;
-	else if ( REFER_NAMESPACE_QPOASES isEqual(*BETA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
-		for (k = 0; k < *N; k++)
-			for (j = 0; j < *M; j++)
-				C[j+(*LDC)*k] = -C[j+(*LDC)*k];
-	else if ( REFER_NAMESPACE_QPOASES isEqual(*BETA,1.0) == REFER_NAMESPACE_QPOASES BT_FALSE )
-		for (k = 0; k < *N; k++)
-			for (j = 0; j < *M; j++)
-				C[j+(*LDC)*k] *= *BETA;
+// 	if ( REFER_NAMESPACE_QPOASES isZero(*BETA) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 		for (k = 0; k < *N; k++)
+// 			for (j = 0; j < *M; j++)
+// 				C[j+(*LDC)*k] = 0.0;
+// 	else if ( REFER_NAMESPACE_QPOASES isEqual(*BETA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 		for (k = 0; k < *N; k++)
+// 			for (j = 0; j < *M; j++)
+// 				C[j+(*LDC)*k] = -C[j+(*LDC)*k];
+// 	else if ( REFER_NAMESPACE_QPOASES isEqual(*BETA,1.0) == REFER_NAMESPACE_QPOASES BT_FALSE )
+// 		for (k = 0; k < *N; k++)
+// 			for (j = 0; j < *M; j++)
+// 				C[j+(*LDC)*k] *= *BETA;
 
-	if (TRANSA[0] == 'N')
-		if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] += A[j+(*LDA)*i] * B[i+(*LDB)*k];
-		else if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] -= A[j+(*LDA)*i] * B[i+(*LDB)*k];
-		else
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] += *ALPHA * A[j+(*LDA)*i] * B[i+(*LDB)*k];
-	else
-		if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] += A[i+(*LDA)*j] * B[i+(*LDB)*k];
-		else if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] -= A[i+(*LDA)*j] * B[i+(*LDB)*k];
-		else
-			for (k = 0; k < *N; k++)
-				for (j = 0; j < *M; j++)
-					for (i = 0; i < *K; i++)
-						C[j+(*LDC)*k] += *ALPHA * A[i+(*LDA)*j] * B[i+(*LDB)*k];
-}
+// 	if (TRANSA[0] == 'N')
+// 		if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] += A[j+(*LDA)*i] * B[i+(*LDB)*k];
+// 		else if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] -= A[j+(*LDA)*i] * B[i+(*LDB)*k];
+// 		else
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] += *ALPHA * A[j+(*LDA)*i] * B[i+(*LDB)*k];
+// 	else
+// 		if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] += A[i+(*LDA)*j] * B[i+(*LDB)*k];
+// 		else if ( REFER_NAMESPACE_QPOASES isEqual(*ALPHA,-1.0) == REFER_NAMESPACE_QPOASES BT_TRUE )
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] -= A[i+(*LDA)*j] * B[i+(*LDB)*k];
+// 		else
+// 			for (k = 0; k < *N; k++)
+// 				for (j = 0; j < *M; j++)
+// 					for (i = 0; i < *K; i++)
+// 						C[j+(*LDC)*k] += *ALPHA * A[i+(*LDA)*j] * B[i+(*LDB)*k];
+// }
 
 extern "C" void sgemm_(	const char* TRANSA, const char* TRANSB,
 						const la_uint_t* M, const la_uint_t* N, const la_uint_t* K,
